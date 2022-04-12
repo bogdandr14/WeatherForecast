40fe0d9b697f390f99e4f88bc9636be8a98287c1
Dorobantu Robert <dorobantu.robert.c5a@student.ucv.ro>
2022-04-12 20:16:17 +0300

    add current location to favorites

--- app/src/main/java/com/cm/weatherforecast/activities/LocationManagerActivity.java
+++ app/src/main/java/com/cm/weatherforecast/activities/LocationManagerActivity.java
@@ -1,11 +1,23 @@
 
 package com.cm.weatherforecast.activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cm.weatherforecast.Constants;
@@ -14,10 +26,23 @@ import com.cm.weatherforecast.R;
 import com.cm.weatherforecast.WeatherChecker;
 import com.cm.weatherforecast.adapters.LocationAdapter;
 import com.cm.weatherforecast.modals.LocationModal;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 public class LocationManagerActivity extends AppCompatActivity {
@@ -29,6 +54,9 @@ public class LocationManagerActivity extends AppCompatActivity {
 
     private List<LocationModal> locationsList;
     private LocationAdapter locationAdapter;
+    private LocationRequest locationRequest;
+
+    private String cityName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +67,111 @@ public class LocationManagerActivity extends AppCompatActivity {
         citySearchTIET = findViewById(R.id.idCitySearchTIET);
         cityAddIV = findViewById(R.id.idCityAddIV);
         interestLocationsRV = findViewById(R.id.idInterestLocationsRV);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+
         setStoredLocations();
         setListener();
+
+        getCurrentLocation();
     }
 
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = null;
+        boolean isEnabled = false;
+        if (locationManager == null) {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        }
+        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
+    }
+
+    private String getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(LocationManagerActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION}, Constants.PERMISSION_CODE);
+        }
+        if (isGPSEnabled()) {
+            LocationServices.getFusedLocationProviderClient(LocationManagerActivity.this).requestLocationUpdates(locationRequest, new LocationCallback() {
+                @Override
+                public void onLocationResult(@NonNull LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    LocationServices.getFusedLocationProviderClient(LocationManagerActivity.this).removeLocationUpdates(this);
+                    if (locationResult != null && locationResult.getLocations().size() > 0) {
+                        int index = locationResult.getLocations().size() - 1;
+                        Location location = locationResult.getLocations().get(index);
+                        cityName = getLocationCityName(location.getLatitude(), location.getLongitude());
+                    }
+                    locationsList.add(new LocationModal(cityName.toString(), "35"));
+                }
+            }, Looper.getMainLooper());
+        } else {
+            turnOnGPS();
+            cityName = Constants.CITY;
+        }
+        return cityName;
+    }
+
+    private void turnOnGPS() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(LocationManagerActivity.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(LocationManagerActivity.this, 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
+    }
+
+    private String getLocationCityName(double latitude, double longitude) {
+        String cityName = getString(R.string.city_not_found);
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = gcd.getFromLocation(latitude, longitude, 10);
+            for (Address adr : addresses) {
+                if (adr != null) {
+                    String city = adr.getLocality();
+                    if (city != null && !city.equals("")) {
+                        cityName = city;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cityName;
+    }
 
     private void setStoredLocations() {
         locationsList = new ArrayList<>();
